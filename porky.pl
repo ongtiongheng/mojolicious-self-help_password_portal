43f26e70d320da2e61859b569f6d55ff9c819aa1
Patrick Wayne <ongtiongheng@gmail.com>
2018-12-24 12:07:30 +0800

    Update porky.pl

--- porky.pl
+++ porky.pl
@@ -12,7 +12,7 @@ sub user_exists {
  my ($username, $password) = @_;
  my $radius = Authen::Simple::RADIUS->new(
      host   => 'localhost',
-     secret => 'IDontKnowWhat2Say'
+     secret => '<password>'
    );
  
    # successfull authentication
